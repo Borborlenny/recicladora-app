1e36b3e8832705b2297c16aa5bcb3a40d1cba080
Saúl Maldonado lópez <Alejandrom199916@gmail.com>
2024-11-21 18:14:46 -0500

    Arreglos de configuración

--- recicladora-app/src/app/app.component.ts
+++ recicladora-app/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CrudUsuarioComponent } from './components/crud-usuario/crud-usuario.component';
-import { CrudMaterialComponent } from './components/crud-material/crud-material.component';
-import { CrudEmpresaComponent } from './components/crud-empresa/crud-empresa.component';
-import { CrudRutaComponent } from './components/crud-ruta/crud-ruta.component';
-import { CrudHogarComponent } from './components/crud-hogar/crud-hogar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink,CrudUsuarioComponent, CrudMaterialComponent, CrudEmpresaComponent, CrudRutaComponent,CrudHogarComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- recicladora-app/src/app/app.routes.ts
+++ recicladora-app/src/app/app.routes.ts
@@ -1,39 +1,36 @@
 import { Routes } from '@angular/router';
+import { CrudUsuarioComponent } from './components/crud-usuario/crud-usuario.component';
 
 
 export const routes: Routes = [
-    {
-        path: 'usuario',
-        loadComponent: ()=>
-            import('./components/crud-usuario/crud-usuario.component')
-            .then(c => c.CrudUsuarioComponent)
-    },
-
-    {
-        path: 'hogar',
-        loadComponent: ()=>
-            import('./components/crud-hogar/crud-hogar.component')
-            .then(c => c.CrudHogarComponent)
-    },
-
-    {
-        path: 'material',
-        loadComponent: ()=>
-            import('./components/crud-material/crud-material.component')
-            .then(c => c.CrudMaterialComponent)
-    },
-
-    {
-        path: 'empresa',
-        loadComponent: ()=>
-            import('./components/crud-empresa/crud-empresa.component')
-            .then(c => c.CrudEmpresaComponent)
-    },
-
-    {
-        path: 'ruta',
-        loadComponent: ()=>
-            import('./components/crud-ruta/crud-ruta.component')
-            .then(c => c.CrudRutaComponent)
-    }
+  {
+    path: 'usuario',
+    loadComponent: () =>
+      import('./components/crud-usuario/crud-usuario.component')
+    .then(c => c.CrudUsuarioComponent)
+  },
+  {
+    path: 'hogar',
+    loadComponent: () =>
+      import('./components/crud-hogar/crud-hogar.component')
+    .then(c => c.CrudHogarComponent)
+  },
+  {
+    path: 'material',
+    loadComponent: () =>
+      import('./components/crud-material/crud-material.component')
+    .then(c => c.CrudMaterialComponent)
+  },
+  {
+    path: 'empresa',
+    loadComponent: () =>
+      import('./components/crud-empresa/crud-empresa.component')
+    .then(c => c.CrudEmpresaComponent)
+  },
+  {
+    path: 'ruta',
+    loadComponent: () =>
+      import('./components/crud-ruta/crud-ruta.component')
+    .then(c => c.CrudRutaComponent)
+  },
 ];
