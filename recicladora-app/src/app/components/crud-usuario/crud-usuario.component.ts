0246c39b7ba7a4501b776c0246a34565e13279cc
Saúl Maldonado lópez <Alejandrom199916@gmail.com>
2024-11-21 20:18:57 -0500

    Final usuario

--- recicladora-app/src/app/components/crud-usuario/crud-usuario.component.ts
+++ recicladora-app/src/app/components/crud-usuario/crud-usuario.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {UsuarioJsonService} from '../../services/usuario-json.service';
 import {Usuario} from '../../models/Usuario';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 import {MatTableDataSource} from '@angular/material/table'
 import {MatPaginator} from '@angular/material/paginator';
 import {MatFormField} from '@angular/material/form-field';
@@ -10,11 +10,12 @@ import {MatInputModule} from '@angular/material/input'
 import { MatOption } from '@angular/material/core';
 import { MatSelect } from '@angular/material/select';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-crud-usuario',
-  imports: [MatFormField, MatLabel, ReactiveFormsModule, MatInputModule, MatOption, MatSelect, MatCheckboxModule],
+  imports: [MatFormField, MatLabel, ReactiveFormsModule, MatInputModule, MatOption, MatSelect, MatCheckboxModule, MatPaginator, MatTableModule, MatButtonModule],
   templateUrl: './crud-usuario.component.html',
   styleUrl: './crud-usuario.component.css'
 })
@@ -32,21 +33,94 @@ export class CrudUsuarioComponent implements OnInit, AfterViewInit{
   constructor(private usuarioService: UsuarioJsonService, private fb: FormBuilder){
 
   }
- 
+
   ngOnInit(): void {
     this.obtenerUsuarios()
 
     this.form =this.fb.group({
-      nombre: [""],
-      email: [""],
-      rol: [""],
-      estado: [""]
+      nombre: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      rol: ["", [Validators.required]],
+      estado: ["", [Validators.required]]
     })
   }
 
   obtenerUsuarios(){
     this.usuarioService.obtenerUsuarios().subscribe((data: Usuario[])=>{
-        console.log(data)
+        this.dataSource.data = data;
+    })
+  }
+
+  search(searchInput: HTMLInputElement){
+    if(searchInput.value){
+      this.usuarioService.obtenerUsuarioBusqueda(searchInput.value).subscribe((datos: Usuario[]) => {
+        this.dataSource.data = datos;
+      })
+    }
+    else{
+      this.obtenerUsuarios();
+    }
+  }
+
+  onSubmit(){
+    if(this.form.invalid){
+      return;
+    }
+
+    //Obtener los datos de los controles del formulario
+    const newUsuario: Usuario = this.form.value;
+
+    if(this.isEditMode){ //Actualizar hogar
+      newUsuario.id = this.currentId;
+      this.usuarioService.actualizarUsuario(newUsuario).subscribe((actualizarUsuario)=>{
+        alert("Usuario fue editada exitosamente");
+        this.obtenerUsuarios(); //Actualizar el dataSource
+      });
+    }
+    else{ //Agregar hogar
+      this.usuarioService.agregarUsuario(newUsuario).subscribe((agregarUsuario)=>{
+        alert("Usuario fue agregada exitosamente");
+        this.obtenerUsuarios(); //Actualizar el dataSource
+      });
+    }
+
+    this.clearForm();
+  }
+
+  clearForm(){
+    this.form.reset({
+      nombre: "",
+      email: "",
+      rol: "",
+      estado: ""
+    })
+
+    this.currentId = 0;
+    this.isEditMode = false;
+  }
+
+  eliminar(usuario:Usuario){
+    this.usuarioService.eliminarUsuario(usuario).subscribe(()=>{
+      alert("Eliminado exitosamente.")
+      this.obtenerUsuarios();
+    })
+  }
+
+  editar(usuario:Usuario){
+    this.isEditMode = true;
+
+    if(usuario && usuario.id){
+      this.currentId = usuario.id;
+    }
+    else{
+      console.log("Usuario o el id del usuario estan undefined")
+    }
+
+    this.form.setValue({
+      nombre: usuario.nombre,
+      email: usuario.email,
+      rol: usuario.rol,
+      estado: usuario.estado
     })
   }
 }
--- recicladora-app/src/app/services/usuario-json.service.ts
+++ recicladora-app/src/app/services/usuario-json.service.ts
@@ -2,14 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Usuario } from '../models/Usuario';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioJsonService {
+
   private jsonUrl: string ="http://localhost:3000/usuario"
+
   constructor(private http: HttpClient) { }
+
+  //Obtener todos los Usuarios
   obtenerUsuarios():Observable<Usuario[]>{
       return this.http.get<Usuario[]>(this.jsonUrl)
+  }
+
+  //Obtener busqueda de Usuarios
+  obtenerUsuarioBusqueda(nombre?: string, email?:string):Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.jsonUrl).pipe(
+      map((usuarios) =>
+        usuarios.filter((usuario) =>
+          (nombre ? usuario.nombre.toLowerCase().includes(nombre.toLowerCase()):true) &&
+          (email ? usuario.email.toLowerCase().includes(email.toLowerCase()):true)
+        )
+      )
+    );
+  }
+
+  //Agregar un Usuario
+  agregarUsuario(usuario: Usuario):Observable<Usuario>{
+    return this.http.post<Usuario>(this.jsonUrl, usuario);
+  }
+
+  //actualizar un Usuario
+  actualizarUsuario(usuario: Usuario):Observable<Usuario>{
+    const urlDeUsuario = `${this.jsonUrl}/${usuario.id}`
+    return this.http.put<Usuario>(urlDeUsuario, usuario);
+  }
+
+  //eliminar un Usuario
+  eliminarUsuario(usuario: Usuario):Observable<void>{
+    const urlDeUsuario = `${this.jsonUrl}/${usuario.id}`
+    return this.http.delete<void>(urlDeUsuario);
   }
 }
